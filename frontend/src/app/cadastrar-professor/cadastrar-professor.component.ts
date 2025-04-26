0c02a7cd29306ccf06c7c8cb8d68682723ed5047
Augusto <asergioscosta@gmail.com>
2025-04-26 16:37:11 -0300

    Realizar alterações no HTML, CSS e ts dos componentes professor e cursos.

--- frontend/src/app/cadastrar-professor/cadastrar-professor.component.ts
+++ frontend/src/app/cadastrar-professor/cadastrar-professor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastrar-professor',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrl: './cadastrar-professor.component.scss'
 })
 export class CadastrarProfessorComponent {
+  cep: string = '';
+  logradouro: string = '';
+  bairro: string = '';
+  complemento: string = '';
+  cidade: string = '';
+  uf: string = '';
+  numero: string = '';
 
+  constructor(private http: HttpClient) { }
+
+  formatarCep(cep: string): string {
+    return cep.replace(/^(\d{5})(\d{3})$/, '$1-$2');
+  }
+
+  buscarCep() {
+    if (!this.cep) return;
+
+    this.http.get<any>(`https://brasilapi.com.br/api/cep/v2/${this.cep}`)
+      .subscribe({
+        next: (data) => {
+          this.logradouro = data.street;
+          this.bairro = data.neighborhood;
+          this.cidade = data.city;
+          this.uf = data.state;
+          this.cep = this.formatarCep(data.cep);
+        },
+        error: (err) => {
+          alert('Erro ao buscar CEP.');
+          console.error(err);
+        }
+      });
+  }
+
+  mostrarResponsavel: boolean = false;
+  responsaveisDisponiveis: any[] = [];
+  responsavelSelecionado: string = '';
+
+  onResponsavelChange(event: any) {
+    this.responsavelSelecionado = event.target.value;
+  }
 }
